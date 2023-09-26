645b7c1634235c763c197df90f4b7087adf6bb54
abette7 <104946351+abette7@users.noreply.github.com>
2023-09-26 15:01:40 -0400

    Update Strip2PatternsAndBetweenFromFileName.py

--- Strip2PatternsAndBetweenFromFileName.py
+++ Strip2PatternsAndBetweenFromFileName.py
@@ -6,7 +6,7 @@ import re
 ### If your file names use brackets select another pair of place
 ### holder characters for the three newFile_name assignments below  
 
-myFolder = (r'/Users/adam.betterton/Desktop/untitled folder 8')
+myFolder = (r'/Path to directory for renaming files')
 startPattern = '_('
 endPattern = '_m'
 
