f7ab190d58b342a21ef011c34882c0238d63bab3
abette7 <104946351+abette7@users.noreply.github.com>
2023-09-26 14:58:02 -0400

    Update Strip2PatternsAndBetweenFromFileName.py

--- Strip2PatternsAndBetweenFromFileName.py
+++ Strip2PatternsAndBetweenFromFileName.py
@@ -6,7 +6,7 @@ import re
 ### If your file names use brackets select another pair of place
 ### holder characters for the three newFile_name assignments below  
 
-myFolder = (r'/Path to directory with files to rename')
+myFolder = (r'/Users/adam.betterton/Desktop/untitled folder 8')
 startPattern = '_('
 endPattern = '_m'
 
@@ -14,8 +14,8 @@ for dirpath, dirnames, filenames in os.walk(myFolder):
     for file_name in filenames:
         file_path = os.path.join(dirpath, file_name)
         #print(file_name)
-        newFile_name = file_name.replace('_(', '[')
-        newFile_name = newFile_name.replace('_m', ']')
+        newFile_name = file_name.replace(startPattern, '[')
+        newFile_name = newFile_name.replace(endPattern, ']')
         newFile_name = re.sub(r'\[.*?\]', '', newFile_name)
         #print(newFile_name)
         newFile_path = os.path.join(dirpath, newFile_name)
