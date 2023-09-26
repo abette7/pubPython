0ddd53aa98f8535943fccd6a5dd0ee356d37696a
abette7 <104946351+abette7@users.noreply.github.com>
2023-09-26 14:50:58 -0400

    Add files via upload

---
+++ Strip2PatternsAndBetweenFromFileName.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import re
+
+### This script uses brackets as placeholder characters.
+### If your file names use brackets select another pair of place
+### holder characters for the three newFile_name assignments below  
+
+myFolder = (r'/Path to directory with files to rename')
+startPattern = '_('
+endPattern = '_m'
+
+for dirpath, dirnames, filenames in os.walk(myFolder):
+    for file_name in filenames:
+        file_path = os.path.join(dirpath, file_name)
+        #print(file_name)
+        newFile_name = file_name.replace('_(', '[')
+        newFile_name = newFile_name.replace('_m', ']')
+        newFile_name = re.sub(r'\[.*?\]', '', newFile_name)
+        #print(newFile_name)
+        newFile_path = os.path.join(dirpath, newFile_name)
+        shutil.move(file_path, newFile_path)
